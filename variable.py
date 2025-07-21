39108ae5889754f0ad37824d897bac2fec52954c
debanjan19974 <42506685+debanjandas1997@users.noreply.github.com>
2025-07-21 10:14:44 +0530

    Add initial variable definitions and print statements

---
+++ variable.py
@@ -0,0 +1,13 @@
+name = "Debanjan"
+age = 25
+location = "India"
+hobbies = ["reading", "coding", "traveling"]
+is_student = True                   
+is_employed = False
+# variable.py
+print("Name:", name)
+print("Age:", age)
+print("Location:", location)
+print("Hobbies:", ", ".join(hobbies))
+print("Is Student:", is_student)
+print("Is Employed:", is_employed)
